9aa5c87095bb703b8112841e73b3b890858f4253
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2023-01-19 12:58:15 +0545

    created new list class of my own with box decoration

--- lib/Mylist/listViewClassMethode.dart
+++ lib/Mylist/listViewClassMethode.dart
@@ -1,3 +1,4 @@
+import 'package:class_1/Mylist/my_list.dart';
 import 'package:flutter/material.dart';
 
 class MyList2 extends StatefulWidget {
@@ -8,11 +9,10 @@ class MyList2 extends StatefulWidget {
 }
 
 class _MyList2State extends State<MyList2> {
-
-  List<String>itemList=[
-    'item 1',
-    'item 2',
-    'item 3',
+  List<String> itemList = [
+    'Aavash',
+    'Gyawali',
+    'Imadol',
     'item 4',
     'item 5',
     'item 1',
@@ -36,15 +36,54 @@ class _MyList2State extends State<MyList2> {
     'item 4',
     'item 5',
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-      ListView.builder(
+      body: ListView.builder(
           itemCount: itemList.length,
-          itemBuilder: (context,index){
-              return Text(itemList[index]);
+          itemBuilder: (context, index) {
+            return MyList3(
+              title: itemList[index],
+            );
           }),
+    );
+  }
+}
+
+class MyList3 extends StatelessWidget {
+  const MyList3({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            decoration:  BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: const LinearGradient(
+                    colors: [
+                      Colors.blue,
+                      Colors.red,
+                    ],
+                    stops: [0.0, 1.0],
+                    begin: FractionalOffset.topCenter,
+                    end: FractionalOffset.bottomCenter,
+                    tileMode: TileMode.repeated
+                )
+            ),
+            padding: const EdgeInsets.all(20),
+            width: double.infinity,
+            height: 100,
+            child: Center(child: Text(title,style: const TextStyle( fontSize: 25),)),
+          ),
+        ),
+
+      ],
     );
   }
 }
