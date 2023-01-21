5c071ada685cfa6492ad6d2318e0c995911d7cb3
Aavash Gyawali <aavash.076bct003@acem.edu.np>
2023-01-21 20:32:18 +0545

    checkbox aligned to the correct postion and the keybord goes away on tapping outside

--- lib/practice/Sign_Up.dart
+++ lib/practice/Sign_Up.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'hlw2_using_regx.dart';
+
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
 
   @override
   State<SignUp> createState() => _SignUpState();
 }
-
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
@@ -16,22 +17,18 @@ class _SignUpState extends State<SignUp> {
         title: const Text("Sign up"),
         backgroundColor: const Color(0XFF643E95),
       ),
-      
       body: 
       Center(
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
-
               backgroundColor: const Color(0XFF00C088)
-
           ),
           onPressed: (){
+
             Navigator.pop(context);
           }, child: const Text("Go Back"),
         ),
       ),
-      
-      
     );
   }
 }
--- lib/practice/hlw2_using_regx.dart
+++ lib/practice/hlw2_using_regx.dart
@@ -8,28 +8,29 @@ class Hlw2Regx extends StatefulWidget {
 }
 
 class _Hlw2RegxState extends State<Hlw2Regx> {
-  bool? check1 = true, signUP=false,signUp1=false;
-  final _formKey= GlobalKey<FormState>();
+  bool? check1 = true, signUP = false, signUp1 = false;
+  final _formKey = GlobalKey<FormState>();
   RegExp pass_valid = RegExp(r"((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)){10,}");
-  RegExp pass_valid_email= RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-  bool validatePassword(String pass){
+  RegExp pass_valid_email = RegExp(
+      r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+  bool validatePassword(String pass) {
     String _password = pass.trim();
-    if(pass_valid.hasMatch(_password)){
+    if (pass_valid.hasMatch(_password)) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
 
-  bool validateEmail(String pass){
+  bool validateEmail(String pass) {
     String _email = pass.trim();
-    if(pass_valid_email.hasMatch(_email)){
+    if (pass_valid_email.hasMatch(_email)) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -40,44 +41,78 @@ class _Hlw2RegxState extends State<Hlw2Regx> {
         // centerTitle: true,
         title: const Text("Login Page"),
       ),
-      body: Container(
-        height: 800,
-        width: 500,
-        margin: const EdgeInsets.only(top: 30, left: 20, right: 20, bottom: 30),
-        padding: const EdgeInsets.only(top: 25, left: 30, right: 30),
-        decoration: const BoxDecoration(),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _welcomeText(),//prints Welcome
-              _latisha_betts(),//prints Latisha Betts
-              _textEmail(),// a text named email is printed
-              _sizeBox(),//size box of 20 height
-              _textFormFieldOfEmail(),// a text Field for Email is produced
-              _textPassword(),// a text named password is printed
-              _textFormFieldOfPassword(),//a text Field for password is produced
-              _sizeBox(),//size box of 20 height
+      body:
+      GestureDetector(
+        onTap: (){
+          FocusScope.of(context).requestFocus(new FocusNode());//on tapping anywhere outside keyboard the keybord goes away
+        },
+        child: Container(
+          // height: 800,
+          // width: 500,
 
-              Row(
-                children: [
-                  _checkBox(),//check box with Remember me is created
-                  _textButtonForgetPassword(),// Forget me text button is created
-                ],
-              ),
-              _sizeBox(),//size box of 20 height
-              _signInButton(),
-              _sizeBox(),//size box of 20 height
-              _sizeBox(),//size box of 20 height
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _textDontHaveAccount(),
-                  _textButtonSignUp(),
-                ],
-              ),
-            ],
+          margin: const EdgeInsets.only(top: 30, left: 20, right: 20, bottom: 30),
+          padding: const EdgeInsets.only(top: 25, left: 30, right: 30),
+          decoration: const BoxDecoration(
+            color: Color(0xffFCFBFD)
+          ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _welcomeText(),
+                //prints Welcome
+                _latisha_betts(),
+                //prints Latisha Betts
+                _textEmail(),
+                // a text named email is printed
+                _sizeBox(),
+                //size box of 20 height
+                _textFormFieldOfEmail(),
+                // a text Field for Email is produced
+                _textPassword(),
+                // a text named password is printed
+                _textFormFieldOfPassword(),
+                //a text Field for password is produced
+                _sizeBox(),
+                //size box of 20 height
+
+                Row(
+                  children: [
+                    _checkBox(),
+                    //check box with Remember me is created
+                    Container(
+                        margin: const EdgeInsets.only(left: 10, right: 30),
+                        padding: const EdgeInsets.only(
+                          right: 25,
+                        ),
+                        child: const Text(
+                          "Remember Me",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 14),
+                        )),
+                    _textButtonForgetPassword(),
+                    // Forget me text button is created
+                  ],
+                ),
+                _sizeBox(),
+                //size box of 20 height
+                _signInButton(),
+                _sizeBox(),
+                //size box of 20 height
+                _sizeBox(),
+                //size box of 20 height
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _textDontHaveAccount(),
+                    _textButtonSignUp(),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -97,9 +132,7 @@ class _Hlw2RegxState extends State<Hlw2Regx> {
     return const Text(
       "Latisha Betts.",
       style: TextStyle(
-          color: Color(0XFF00C088),
-          fontSize: 40,
-          fontWeight: FontWeight.bold),
+          color: Color(0XFF00C088), fontSize: 40, fontWeight: FontWeight.bold),
     );
   }
 
@@ -108,9 +141,7 @@ class _Hlw2RegxState extends State<Hlw2Regx> {
       margin: const EdgeInsets.only(top: 30),
       child: const Text("Email",
           style: TextStyle(
-              color: Colors.grey,
-              fontSize: 17,
-              fontWeight: FontWeight.w500)),
+              color: Colors.grey, fontSize: 17, fontWeight: FontWeight.w500)),
     );
   }
 
@@ -122,37 +153,27 @@ class _Hlw2RegxState extends State<Hlw2Regx> {
 
   _textFormFieldOfEmail() {
     return TextFormField(
-      validator: (value1){
-        if(value1!.isEmpty){
-          return "plese enter Email";
-
-        }
-        else{
+      validator: (value1) {
+        if (value1!.isEmpty) {
+          return "plese enter email";
+        } else {
           bool result1 = validateEmail(value1);
-          if (result1){
-            signUp1=true;
-
-          }
-          else{
+          if (result1) {
+            signUp1 = true;
+          } else {
             return "Write Email in abc@xyz.com form";
           }
         }
-
       },
-
       decoration: const InputDecoration(
-        hintText: "Enter Your Email",
-        hintStyle: TextStyle(
-            color: Colors.grey
-        ),
+        hintText: "Enter Your email",
+        hintStyle: TextStyle(color: Colors.grey),
         fillColor: Colors.green,
         // labelText: "Enter your Email",
         enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey,width:1)),
-        focusedBorder:  OutlineInputBorder(
-
-          borderSide:  BorderSide(color: Color(0XFF00C088),width: 3 ),
-
+            borderSide: BorderSide(color: Colors.grey, width: 1)),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0XFF00C088), width: 3),
         ),
       ),
     );
@@ -163,80 +184,82 @@ class _Hlw2RegxState extends State<Hlw2Regx> {
       margin: const EdgeInsets.only(top: 30, bottom: 20),
       child: const Text("Password",
           style: TextStyle(
-              color: Colors.grey,
-              fontSize: 17,
-              fontWeight: FontWeight.w500)),
+              color: Colors.grey, fontSize: 17, fontWeight: FontWeight.w500)),
     );
   }
 
   _textFormFieldOfPassword() {
     return TextFormField(
-      validator: (value){
-        if(value!.isEmpty){
+      validator: (value) {
+        if (value!.isEmpty) {
           return "plese enter a password";
-
-        }
-        else{
+        } else {
           bool result = validatePassword(value);
-          if (result){
-            signUP=true;
-
-          }
-          else{
+          if (result) {
+            signUP = true;
+          } else {
             return "Password should contain Capital, small letter & \nNumber & Special character with atleast 10";
           }
         }
-
       },
       obscureText: true,
       decoration: const InputDecoration(
-        hintStyle: TextStyle(
-            color: Colors.grey
-        ),
+        hintStyle: TextStyle(color: Colors.grey),
         // labelText: "Password",
         hintText: "Password",
         enabledBorder: OutlineInputBorder(
             borderSide: BorderSide(color: Colors.grey, width: 1)),
-        focusedBorder:  OutlineInputBorder(
-
-          borderSide:  BorderSide(color: Color(0XFF00C088),width: 3 ),
-
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color(0XFF00C088), width: 3),
         ),
       ),
     );
   }
 
   _checkBox() {
-    return Expanded(
-      child: CheckboxListTile(
-
-        contentPadding: const EdgeInsets.only(right: 20.0),
-        //checkbox positioned at left
-
-        value: check1,
-        activeColor: const Color(0XFF00C088),
-        checkColor: Colors.white,
-
-
-        controlAffinity: ListTileControlAffinity.leading,
-        onChanged: (bool? value) {
-          setState(() {
-            check1 = value;
-          });
-        },
-        title: const Text("Remember Me",
-
-            style: TextStyle(
-                color: Colors.grey,
-                fontSize: 15,
-                fontWeight: FontWeight.w500)),
-      ),
+    // return Expanded(
+    //   child: CheckboxListTile(
+    //       contentPadding: EdgeInsets.zero,
+    //
+    //     //checkbox positioned at left
+    //
+    //     value: check1,
+    //     activeColor: const Color(0XFF00C088),
+    //     checkColor: Colors.white,
+    //
+    //
+    //     controlAffinity: ListTileControlAffinity.leading,
+    //     onChanged: (bool? value) {
+    //       setState(() {
+    //         check1 = value;
+    //       });
+    //     },
+    //     title: const Text("Remember Me",
+    //
+    //         style: TextStyle(
+    //             color: Colors.grey,
+    //             fontSize: 15,
+    //             fontWeight: FontWeight.w500)),
+    //   ),
+    // );
+    return SizedBox(
+      height: 20.0,
+      width: 20.0,
+      child: Checkbox(
+          //only check box
+          value: check1,
+          activeColor: const Color(0XFF00C088), //unchecked
+          onChanged: (bool? value) {
+            //value returned when checkbox is clicked
+            setState(() {
+              check1 = value;
+            });
+          }),
     );
   }
 
   _textButtonForgetPassword() {
     return TextButton(
-
         style: TextButton.styleFrom(
           padding: const EdgeInsets.only(left: 20),
           textStyle: const TextStyle(fontSize: 15),
@@ -248,23 +271,21 @@ class _Hlw2RegxState extends State<Hlw2Regx> {
           "Forgot Password",
           style: TextStyle(
             color: Colors.red,
+
           ),
         ));
   }
 
   _signInButton() {
     return ElevatedButton(
-
       style: ElevatedButton.styleFrom(
-          fixedSize:const Size(1000, 60),
-          backgroundColor: const Color(0XFF00C088)
-
-      ),
+          fixedSize: const Size(1000, 60),
+          backgroundColor: const Color(0XFF00C088)),
       onPressed: () {
-
         _formKey.currentState!.validate();
-        if (signUP==true && signUp1==true) {
+        if (signUP == true && signUp1 == true) {
           Navigator.pushNamed(context, '/second');
+          signUP = signUp1 = false;
         }
         //pachadi jaanako laagi
       },
@@ -276,9 +297,7 @@ class _Hlw2RegxState extends State<Hlw2Regx> {
     return const Text(
       "Don't have account?",
       style: TextStyle(
-          color: Colors.grey,
-          fontWeight: FontWeight.w500,
-          fontSize: 15),
+          color: Colors.grey, fontWeight: FontWeight.w500, fontSize: 15),
     );
   }
 
@@ -288,12 +307,13 @@ class _Hlw2RegxState extends State<Hlw2Regx> {
           textStyle: const TextStyle(fontSize: 15),
         ),
         onPressed: () {
-
           Navigator.pushNamed(context, '/signup');
         },
         child: const Text(
           "Sign Up",
-          style: TextStyle(color: Colors.red),
+          style: TextStyle(
+            color: Colors.red,
+          ),
         ));
   }
 }
